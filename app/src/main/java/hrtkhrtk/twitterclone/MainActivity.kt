46413b2f0052a3d574ba5e5af76fd41d94691f87
hirotaka.iwasaki <nddgs@yahoo.co.jp>
2019-11-25 05:05:33 +0900

    自作課題ver00_201911250500（NavigationDrawerの作成、など）

--- app/src/main/java/hrtkhrtk/twitterclone/MainActivity.kt
+++ app/src/main/java/hrtkhrtk/twitterclone/MainActivity.kt
@@ -9,18 +9,24 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import android.content.Intent
 import android.support.v7.widget.Toolbar
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
 
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var mToolbar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
+        mToolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(mToolbar)
 
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
         fab.setOnClickListener { _ ->
             // ログイン済みのユーザーを取得する
             val user = FirebaseAuth.getInstance().currentUser
@@ -31,6 +37,15 @@ class MainActivity : AppCompatActivity() {
                 startActivity(intent)
             }
         }
+
+        // ナビゲーションドロワーの設定
+        val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
+        val toggle = ActionBarDrawerToggle(this, drawer, mToolbar, R.string.app_name, R.string.app_name)
+        drawer.addDrawerListener(toggle)
+        toggle.syncState()
+
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -47,5 +62,31 @@ class MainActivity : AppCompatActivity() {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+
+        if (id == R.id.nav_posts) {
+            mToolbar.title = "posts"
+        } else if (id == R.id.nav_search_posts) {
+            mToolbar.title = "search_posts"
+        } else if (id == R.id.nav_search_users) {
+            mToolbar.title = "search_users"
+        } else if (id == R.id.nav_followings_list) {
+            mToolbar.title = "followings_list"
+        } else if (id == R.id.nav_followers_list) {
+            mToolbar.title = "followers_list"
+        } else if (id == R.id.nav_favorites_list) {
+            mToolbar.title = "favorites_list"
+        } else if (id == R.id.nav_my_posts) {
+            mToolbar.title = "my_posts"
+        } else if (id == R.id.nav_policy) {
+            mToolbar.title = "policy"
+        }
+
+        val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
+        drawer.closeDrawer(GravityCompat.START)
+        return true
     }
 }
